290904d7e3ab39cd6fd7303eb9eb2f5c5b4594b9
bretthickman <44375617+bretthickman@users.noreply.github.com>
2022-04-16 15:46:33 -0700

    Object Vector Skull Test

--- Final/src/main.cpp
+++ Final/src/main.cpp
@@ -176,8 +176,9 @@ int main(void)
 
     /* Object List */
     vector<Object> objects;
-    objects.push_back(Object(&skullModel, &textureShader, 
-                vec3(1.0f, 1.0f, 0.0f), 0.0f, vec3(0.0f), vec3(1), 1, 1, vec3(0.0f)));
+    for(int i = 0; i<20;i++)
+    objects.push_back(Object(&skullModel, &materialShader, 
+                vec3(-20+(i*2), 1.0f, -4.0f), 0.0f, vec3(1.0f), vec3(1), 1, 1, vec3(1.0f)));          
 
     /* Sound and Lighting */
     ma_engine_play_sound(&musicEngine, "../resources/bach.mp3", NULL);
@@ -236,17 +237,10 @@ int main(void)
             materialShader.setFloat("material.shine", 32.0f); 
 
             lightSystem.Render(materialShader);
-
-            vec3 center = vec3(0.0f, 0.0f, 4.0f);
-            float radius = 1.0f;
-
-            model = mat4(1.0f);
-            model = translate(model, center);
-            materialShader.setMat4("model", model);
-            if(!(frustum.ViewFrustCull(center, radius)))
+            
+            for(int i=0; i<objects.size();i++)
             {
-                box.Draw(materialShader);
-                cout << "Drawing Box!\n";
+                objects[i].Draw();
             }
         }
         materialShader.unbind();
@@ -268,6 +262,7 @@ int main(void)
 
         }    
         textureShader.unbind();
+
 
         /* Render Skybox */
         //nightSkybox.Draw(skyboxShader, camera);
